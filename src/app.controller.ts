24e9b9c35ed4c2e8ae9c1d1e49f0d5200b4e95ec
Dzung Nguyen <dung@hodfords.com>
2024-09-22 00:32:58 +0700

    feat: init api-gateway project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
     @Get()
-    INDEX(): string {
+    index(): string {
         return 'API Gateway';
     }
 }
