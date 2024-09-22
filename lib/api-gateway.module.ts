d2dfa0a6c1290984bfb020bb61e7c178dd0c436c
hodfords-cong-nguyen-be <cong.nc@hodfords.com>
2024-09-22 13:49:32 +0700

    feat: export TooManyRequestException

--- lib/api-gateway.module.ts
+++ lib/api-gateway.module.ts
@@ -1,15 +1,15 @@
-import { DynamicModule, Global, Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { ThrottlerModule } from './throttlers/throttler.module';
 import { RestfulModule } from './restful/restful.module';
 import { ApiGatewayOption } from './types/api-gateway-option.type';
 import { API_GATEWAY_OPTION } from './constants/api-gateway.constant';
 
-@Global()
 @Module({})
 export class ApiGatewayModule {
     static forRoot(option: ApiGatewayOption): DynamicModule {
         option.libraryPath = __dirname;
         return {
+            global: true,
             module: ApiGatewayModule,
             imports: [ThrottlerModule.forRoot(option.throttler), RestfulModule],
             controllers: [],
