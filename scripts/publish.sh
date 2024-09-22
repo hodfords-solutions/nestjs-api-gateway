c3861b815c16960583c115dc5ffec1d993aa7ead
Dzung Nguyen <dung@hodfords.com>
2024-09-22 09:48:10 +0700

    feat: init api-gateway project

--- scripts/publish.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-npm view @hodfords/api-gateway@"$(node -p "require('./package.json').version")" version && echo "Package is already published" && exit 0 || true
-npm install
-npm run build
-cd dist/lib
-npm publish --access public
